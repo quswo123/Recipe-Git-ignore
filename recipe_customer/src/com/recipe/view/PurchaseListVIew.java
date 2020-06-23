713197899663ce71f5b26e7a26c938895c5dde00
quswo123 <quswo123@naver.com>
2020-06-23 15:01:39 +0900

    중간수정

--- recipe_customer/src/com/recipe/view/PurchaseListVIew.java
+++ recipe_customer/src/com/recipe/view/PurchaseListVIew.java
@@ -1,7 +1,54 @@
 package com.recipe.view;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.recipe.io.DataIO;
+import com.recipe.io.Menu;
+import com.recipe.vo.Purchase;
 
 public class PurchaseListVIew {
+	private DataIO dio;
+	private Scanner sc = new Scanner(System.in);
+	
+	public PurchaseListVIew(Socket s) {
+		try {
+			dio = new DataIO(new DataOutputStream(s.getOutputStream()), new DataInputStream(s.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void purchaseView() {
+		try {
+			dio.sendMenu(Menu.PURCHASE_LIST);
+			
+			List<Purchase> list = dio.receivePurchaseList();
+			
+			System.out.println("구매내역");
+			System.out.println("레시피상품명/구매일자/후기등록여부");
+			System.out.println(list.size() + "건의 내역이 출력되었습니다");
+			
+			for(Purchase p : list) {
+				System.out.print(p.getPurchaseCode() + "/");
+				System.out.print(p.getPurchaseDate() + "/");
+			}
+			
+			System.out.println("-이전페이지 | + 다음페이지 | * 메인메뉴");
+			System.out.println("원하시는 번호를 입력하세요");
+			
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
 	
 	
 }
