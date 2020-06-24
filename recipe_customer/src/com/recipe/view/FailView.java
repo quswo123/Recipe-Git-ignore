651f19c0d90a3f47e923714395d078f8b7b69456
SOOJEONG-DESKTO\Soojeong <kosj1109@kakao.com>
2020-06-24 10:34:13 +0900

    즐겨찾기 조회 실패시 Fail msg 추가

--- recipe_customer/src/com/recipe/view/FailView.java
+++ recipe_customer/src/com/recipe/view/FailView.java
@@ -18,7 +18,6 @@ public class FailView {
 	}
 	
 	/**
-<<<<<<< HEAD
 	 * 즐겨찾기 목록 조회 실패 메시지를 출력한다
 	 * @author 고수정
 	 */
@@ -26,13 +25,10 @@ public class FailView {
 		System.out.println(msg);
 	}
 	
-
-=======
-	 * 추천 레시피 실패 메시지를 출력한다
+	 /* 추천 레시피 실패 메시지를 출력한다
 	 * @author 최종국
 	 */
 	public void recommendedRecipe() {
 		System.out.println("추천 레시피 실패");
 	}
->>>>>>> 90e1451617b4b88c7e7cd6165b38d6dd4341f37d
 }
