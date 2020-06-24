7ce8810ce6a490eba7aa820abfc1e6fc06703c63
quswo123 <quswo123@naver.com>
2020-06-24 12:41:05 +0900

    병합 완료

--- recipe_customer/src/com/recipe/view/PurchaseInfoView.java
+++ recipe_customer/src/com/recipe/view/PurchaseInfoView.java
@@ -2,7 +2,15 @@ package com.recipe.view;
 
 public class PurchaseInfoView {
 	
-	public void searchPurchaseInfoView() {
+	public void searchPurchaseInfoView(String purchaseRecipeName) {
 		
+		
+		System.out.println("구매상세내역");
+		
+		System.out.println("레시피명");
+		System.out.println("구매일자");
+		System.out.println("구매수량");
+		System.out.println("총 결제금액");
+		System.out.println("1.레시피로가기 | 2.후기등록하기 | 0.목록으로 | *.메인메뉴");
 	}
 }
--- recipe_customer/src/com/recipe/view/PurchaseListVIew.java
+++ recipe_customer/src/com/recipe/view/PurchaseListVIew.java
@@ -38,50 +38,58 @@ public class PurchaseListVIew {
 			System.out.println("나의 구매내역");
 			System.out.println("["+list.size()+"건의 구매내역이 조회되었습니다 ]");
 			System.out.println("레시피상품명/구매일자/후기등록여부");
-			
-			for(Purchase p : list) {
-				System.out.print(p.getPurchaseDetail().getRecipeInfo().getRecipeName());
-				System.out.print(p.getPurchaseDate());
-				for(Review r : rlist) {
-					String comment = r.getReviewComment();
-					if (p.getCustomerId()==r.getCustomerId()) {
-						if(comment.equals("")) {
-							System.out.print("No");
-						}else {
-							System.out.println("Yes");
+	
+			int i =0;
+			if (list.size() > 5) {
+				viewList(list,i, 0);
+			}
+			else {
+				for(Purchase p : list) {
+					System.out.print(p.getPurchaseDetail().getRecipeInfo().getRecipeName());
+					System.out.print(p.getPurchaseDate());
+					for(Review r : rlist) {
+						String comment = r.getReviewComment();
+						if (p.getCustomerId().equals(r.getCustomerId())) {
+							if(comment.equals("")) {
+								System.out.print("No");
+							}else {
+								System.out.println("Yes");
+							}
 						}
 					}
 				}
+				System.out.println("0:다시검색 | *:메인메뉴");
+				System.out.println("상세구매내역를 보시려면 번호를 입력하세요");
 			}
-			
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}	
 	}
+	
 	private void viewList(List<Purchase> purchaseInfo, int i, int j) {
 		Scanner sc = new Scanner(System.in);
-			
-		for (; i < j+5; i++) {
+
+		for (; i < j + 5; i++) {
 			if (i == purchaseInfo.size()) {
 				i = j - 5;
 				j -= 5;
 				break;
-		}
-			System.out.println(i+1 + " "+ purchaseInfo.get(i).getPurchaseDetail().getRecipeInfo().getRecipeName());
-			
+			}
+			System.out.println(i + 1 + " " + purchaseInfo.get(i).getPurchaseDetail().getRecipeInfo().getRecipeName());
+
 		}
 		System.out.println("-:이전페이지 | +:다음페이지 | *:메인메뉴");
-		System.out.println("상세레시피를 보시려면 번호를 입력하세요");
+		System.out.println("상세구매내역를 보시려면 번호를 입력하세요");
 		String value = sc.nextLine();
 		if (value.equals("+")) {
-			viewList(purchaseInfo, i, j+5);
-		} else if(value.equals("-")) {
+			viewList(purchaseInfo, i, j + 5);
+		} else if (value.equals("-")) {
 			if (i == 5 || i < 10) {
 				viewList(purchaseInfo, 0, 0);
 			}
-			viewList(purchaseInfo, i-10, j-5);
-		} else if(value.equals("*")) {
-			//showRecipeInfoView();
+			viewList(purchaseInfo, i - 10, j - 5);
+		} else if (value.equals("*")) {
+			//메인메뉴로 이동
 		}
 	}
 	
