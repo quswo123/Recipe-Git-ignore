7232912f764e39783a90da831554ed7ccca8b3b7
quswo123 <quswo123@naver.com>
2020-06-25 14:33:09 +0900

    병합

--- recipe_customer/src/com/recipe/view/PurchaseInfoView.java
+++ recipe_customer/src/com/recipe/view/PurchaseInfoView.java
@@ -1,12 +1,14 @@
 package com.recipe.view;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Scanner;
 
 import com.recipe.io.DataIO;
 import com.recipe.share.CustomerShare;
 import com.recipe.vo.Purchase;
+import com.recipe.vo.RecipeInfo;
 import com.recipe.vo.Review;
 
 public class PurchaseInfoView {
@@ -20,18 +22,32 @@ public class PurchaseInfoView {
 	
 	public void searchPurchaseInfoView(Purchase p) {
 		List<Review> rlist = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
+		String menu = null;
 		
 		try {
-			dio.send(p);
+			dio.send(p.getPurchaseDetail().getRecipeInfo());
 			dio.sendId(CustomerShare.loginedId);
 			
 			System.out.println("구매상세내역");
 			System.out.println("레시피명 : " + p.getPurchaseDetail().getRecipeInfo().getRecipeName());
-			System.out.println("구매일자 : " + p.getPurchaseDate());
+			System.out.println("구매일자 : " + sdf.format(p.getPurchaseDate()));
 			System.out.println("구매수량 : " + p.getPurchaseDetail().getPurchaseDetailQuantity());
 			System.out.println("총 결제금액 : " + p.getPurchaseDetail().getPurchaseDetailQuantity()*p.getPurchaseDetail().getRecipeInfo().getRecipePrice());
+
 			System.out.println("1.상세레시피로가기 | 2.후기등록하기 | 0.목록으로");
-			
+			menu = sc.nextLine();
+			if(menu.equals("2")) {
+				
+			}else if(menu.equals("0")) {
+				
+			}else {
+				int n = Integer.parseInt(menu);
+				System.out.println(menu);
+				RecipeInfo param = p.getPurchaseDetail().getRecipeInfo();		
+				RecipeInfoView view = new RecipeInfoView(dio);
+				view.showRecipeInfoView(param);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- recipe_customer/src/com/recipe/view/PurchaseListVIew.java
+++ recipe_customer/src/com/recipe/view/PurchaseListVIew.java
@@ -39,65 +39,57 @@ public class PurchaseListVIew {
 			int end_index = size <= 5 ? size : 5;// 화면에 다섯개씩 보여줄때 사용할 끝 인덱스
 													// ListView를 최초로 구성할때, list의 size가 5 이하이면 size만큼 화면에 출력하고, 5를 초과하면 5만큼만
 													// 화면에 출력
-			
-			for (int i = start_index; i < end_index; i++) {
-				System.out.println("나의 구매내역");
-				System.out.println("[" + list.size() + "건의 구매내역이 조회되었습니다 ]");
-				System.out.println("레시피상품명/구매일자/후기등록여부");
-
-				for (Purchase p : list) {
-					System.out.print(i+1 + ". " + list.get(i).getPurchaseDetail().getRecipeInfo().getRecipeName() + "/ ");
-					System.out.print(sdf.format(list.get(i).getPurchaseDate()) + "/ ");
-					for (Review r : rlist) {
-						if (p.getPurchaseDate().equals(r.getReviewDate()) && p.getPurchaseDetail().getRecipeInfo()
-								.getRecipeCode() == r.getRecipeInfo().getRecipeCode()) {
-							System.out.println("No");
-						} else {
-							System.out.println("Yes");
-						}
+			System.out.println("나의 구매내역");
+			System.out.println("[" + list.size() + "건의 구매내역이 조회되었습니다 ]");
+			System.out.println("레시피상품명/구매일자/후기등록여부");
+			do {
+				for (Purchase p : list) {	
+					for (int i = start_index; i < end_index; i++) {					
+						System.out.print(i+1 + ". " + list.get(i).getPurchaseDetail().getRecipeInfo().getRecipeName() + "/ ");
+						System.out.print(sdf.format(p.getPurchaseDate()) + "/ ");
+							for (Review r : rlist) {
+								if (p.getPurchaseDate().equals(r.getReviewDate()) && p.getPurchaseDetail().getRecipeInfo().getRecipeCode() == r.getRecipeInfo().getRecipeCode()) {
+									System.out.println("No");
+								} else {
+									System.out.println("Yes");
+								}
+							}
 					}
 				}
-			}
+				if(size < 5) {
+					System.out.println("상세페이지번호 : | *.이전화면");
+					menu = sc.nextLine();
+					int n = Integer.parseInt(menu);
+					System.out.println(menu);
+					Purchase param = list.get(n-1);			
+					PurchaseInfoView infoView = new PurchaseInfoView(dio);
+					infoView.searchPurchaseInfoView(param);
+				}
+				else {
+					System.out.println("-:이전페이지 +:다음페이지 | 상세페이지번호 : | *.이전화면");
+					menu = sc.nextLine();
+					if(menu.equals("-")) {
+						start_index = (start_index - 5) >= 0 ? (start_index - 5) : 0; //이전 페이지를 누르면 시작 인덱스 값을 5 감소시킨다. 이떄, 0보다 작아지면 0으로 설정한다
+						end_index = start_index + 5; //시작 인덱스부터 다섯개를 출력하기 위해 끝 인덱스는 시작 인덱스에서 5 증가한 값을 갖는다
+					} else if(menu.equals("+")) {
+						end_index = (end_index + 5) <= size ? (end_index + 5) : size; //다음 페이지를 누르면 end_index 값을 5 증가시킨다. 이때, list의 size보다 커지면 size와 같은 값으로 설정한다
+						start_index = end_index - 5; //시작 인덱스부터 다섯개를 출력하기 위해 시작 인덱스는 끝 인덱스에서 5 감소한 값을 갖는다
+					}  else {
+						int n = Integer.parseInt(menu);
+						System.out.println(menu);
+						Purchase param = list.get(n-1);			
+						PurchaseInfoView infoView = new PurchaseInfoView(dio);
+						infoView.searchPurchaseInfoView(param);
+					}
+				}
+			}while(!menu.equals("*"));
+				
 		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	private void printRecipeList(List<RecipeInfo> list) {
-		String menu;
-		int size = list.size();
-		int start_index = 0; //화면에 다섯개씩 보여줄때 사용할 시작 인덱스
-		int end_index = size <= 5 ? size : 5;//화면에 다섯개씩 보여줄때 사용할 끝 인덱스
-											//ListView를 최초로 구성할때, list의 size가 5 이하이면 size만큼 화면에 출력하고, 5를 초과하면 5만큼만 화면에 출력
-		System.out.println("[" + size + "개의 건이 조회되었습니다]");
-		do {			
-			for(int i = start_index; i < end_index; i++) {
-				System.out.println(i+1 + ". " + list.get(i).getRecipeName());
-			}
-			if(size < 5) {
-				System.out.println("0.뒤로가기");
-				menu = sc.nextLine();
-			}
-			else {
-				System.out.println("-:이전페이지 +:다음페이지 0.뒤로가기");
-				menu = sc.nextLine();
-				if(menu.equals("-")) {
-					start_index = (start_index - 5) >= 0 ? (start_index - 5) : 0; //이전 페이지를 누르면 시작 인덱스 값을 5 감소시킨다. 이떄, 0보다 작아지면 0으로 설정한다
-					end_index = start_index + 5; //시작 인덱스부터 다섯개를 출력하기 위해 끝 인덱스는 시작 인덱스에서 5 증가한 값을 갖는다
-				} else if(menu.equals("+")) {
-					end_index = (end_index + 5) <= size ? (end_index + 5) : size; //다음 페이지를 누르면 end_index 값을 5 증가시킨다. 이때, list의 size보다 커지면 size와 같은 값으로 설정한다
-					start_index = end_index - 5; //시작 인덱스부터 다섯개를 출력하기 위해 시작 인덱스는 끝 인덱스에서 5 감소한 값을 갖는다
-				}  else if (!menu.equals("0")){
-					int n = Integer.parseInt(menu);
-					System.out.println(menu);
-					RecipeInfo param = list.get(n-1);			
-					RecipeInfoView infoView = new RecipeInfoView(dio);
-					infoView.showRecipeInfoView(param);
-				}
-			}
-		} while (!menu.equals("0"));
-	}
 	
 	
 }
