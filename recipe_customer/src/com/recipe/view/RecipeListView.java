3cc72568942572034d55f0d87a5b161055cc106c
quswo123 <quswo123@naver.com>
2020-06-25 09:49:24 +0900

    6/25 9시 49분 병합

--- recipe_customer/src/com/recipe/view/RecipeListView.java
+++ recipe_customer/src/com/recipe/view/RecipeListView.java
@@ -42,35 +42,35 @@ public class RecipeListView {
 	}
 
 	private void viewList(List<RecipeInfo> recipeInfo, int i, int j) {
-		Scanner sc = new Scanner(System.in);
+	      Scanner sc = new Scanner(System.in);
 
-		for (; i < j + 5; i++) {
-			if (i == recipeInfo.size()) {
-				i = j - 5;
-				j -= 5;
-				break;
-			}
-			System.out.println(i + 1 + " " + recipeInfo.get(i).getRecipeName());
+	      for (; i < j + 5; i++) {
+	         if (i == recipeInfo.size()) {
+	            i = j - 5;
+	            j -= 5;
+	            break;
+	         }
+	         System.out.println(i + 1 + " " + recipeInfo.get(i).getRecipeName());
 
-		}
-		System.out.println("-:이전페이지 | +:다음페이지 | 0:다시검색 | *:메인메뉴");
-		System.out.println("상세레시피를 보시려면 번호를 입력하세요");
-		String value = sc.nextLine();
-		if (value.equals("+")) {
-			viewList(recipeInfo, i, j + 5);
-		} else if (value.equals("-")) {
-			if (i == 5 || i < 10) {
-				viewList(recipeInfo, 0, 0);
-			}
-			viewList(recipeInfo, i - 10, j - 5);
+	      }
+	      System.out.println("-:이전페이지 | +:다음페이지 | 0:다시검색 | *:메인메뉴");
+	      System.out.println("상세레시피를 보시려면 번호를 입력하세요");
+	      String value = sc.nextLine();
+	      if (value.equals("+")) {
+	         viewList(recipeInfo, i, j + 5);
+	      } else if (value.equals("-")) {
+	         if (i == 5 || i < 10) {
+	            viewList(recipeInfo, 0, 0);
+	         }
+	         viewList(recipeInfo, i - 10, j - 5);
 
-		} else if (value.equals("0")) {
-			// showRecipeInfoView();
-		} else {
-			int t = Integer.valueOf(value);
-			RecipeInfo param = recipeInfo.get(t-1);			
-			RecipeInfoView infoView = new RecipeInfoView(dio);
-			infoView.showRecipeInfoView(param);
-		}
-	}
+	      } else if (value.equals("0")) {
+	         // showRecipeInfoView();
+	      } else {
+	         int t = Integer.valueOf(value);
+	         RecipeInfo param = recipeInfo.get(t-1);         
+	         RecipeInfoView infoView = new RecipeInfoView(dio);
+	         infoView.showRecipeInfoView(param);
+	      }
+	   }
 }
