51c2187c1adf7b409a0f082ea1408fd720f90d70
quswo123 <quswo123@naver.com>
2020-06-25 12:02:51 +0900

    merge

--- recipe_customer/src/com/recipe/view/PurchaseListVIew.java
+++ recipe_customer/src/com/recipe/view/PurchaseListVIew.java
@@ -20,13 +20,14 @@ import com.recipe.vo.Review;
 
 public class PurchaseListVIew {
 	private DataIO dio;
+	private Scanner sc;
 	
 	public PurchaseListVIew(DataIO dio) {
+		sc = new Scanner(System.in);
 		this.dio = dio;
 	}
 	
 	public void purchaseView() {
-		Scanner sc = new Scanner(System.in);
 		List<Purchase> list = null;
 		List<Review> rlist = null;
 		SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd");
@@ -60,6 +61,41 @@ public class PurchaseListVIew {
 		} catch (IOException | ParseException e) {
 			e.printStackTrace();
 		}	
+	}
+	
+	private void printRecipeList(List<RecipeInfo> list) {
+		String menu;
+		int size = list.size();
+		int start_index = 0; //화면에 다섯개씩 보여줄때 사용할 시작 인덱스
+		int end_index = size <= 5 ? size : 5;//화면에 다섯개씩 보여줄때 사용할 끝 인덱스
+											//ListView를 최초로 구성할때, list의 size가 5 이하이면 size만큼 화면에 출력하고, 5를 초과하면 5만큼만 화면에 출력
+		System.out.println("[" + size + "개의 건이 조회되었습니다]");
+		do {			
+			for(int i = start_index; i < end_index; i++) {
+				System.out.println(i+1 + ". " + list.get(i).getRecipeName());
+			}
+			if(size < 5) {
+				System.out.println("0.뒤로가기");
+				menu = sc.nextLine();
+			}
+			else {
+				System.out.println("-:이전페이지 +:다음페이지 0.뒤로가기");
+				menu = sc.nextLine();
+				if(menu.equals("-")) {
+					start_index = (start_index - 5) >= 0 ? (start_index - 5) : 0; //이전 페이지를 누르면 시작 인덱스 값을 5 감소시킨다. 이떄, 0보다 작아지면 0으로 설정한다
+					end_index = start_index + 5; //시작 인덱스부터 다섯개를 출력하기 위해 끝 인덱스는 시작 인덱스에서 5 증가한 값을 갖는다
+				} else if(menu.equals("+")) {
+					end_index = (end_index + 5) <= size ? (end_index + 5) : size; //다음 페이지를 누르면 end_index 값을 5 증가시킨다. 이때, list의 size보다 커지면 size와 같은 값으로 설정한다
+					start_index = end_index - 5; //시작 인덱스부터 다섯개를 출력하기 위해 시작 인덱스는 끝 인덱스에서 5 감소한 값을 갖는다
+				}  else if (!menu.equals("0")){
+					int n = Integer.parseInt(menu);
+					System.out.println(menu);
+					RecipeInfo param = list.get(n-1);			
+					RecipeInfoView infoView = new RecipeInfoView(dio);
+					infoView.showRecipeInfoView(param);
+				}
+			}
+		} while (!menu.equals("0"));
 	}
 	
 }
