65e2b3802d80e7f3864c2c2d876847c75b9a20a0
SOOJEONG-DESKTO\Soojeong <kosj1109@kakao.com>
2020-06-26 13:56:36 +0900

    리뷰 목록목록

--- recipe_customer/src/com/recipe/view/TestView.java
+++ recipe_customer/src/com/recipe/view/TestView.java
@@ -4,11 +4,11 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.List;
 
 import com.recipe.io.DataIO;
 import com.recipe.share.CustomerShare;
-import com.recipe.vo.Point;
-import com.recipe.vo.RecipeInfo;
+import com.recipe.vo.Review;
 
 /**
  * @author Soojeong 테스트 파일 입니다. upload 금지
@@ -22,25 +22,27 @@ public class TestView {
 		try {
 			s = new Socket("127.0.0.1", 1025);
 			dio = new DataIO(new DataOutputStream(s.getOutputStream()), new DataInputStream(s.getInputStream()));
-			CustomerShare.loginedId = "backym";
-			RecipeInfo info = new RecipeInfo();
-			info.setRecipeCode(135);
-			info.setIngredients(null);
-			info.setRecipeName("유부계란찜");
-			info.setRecipePrice(3000);
-			info.setRecipeProcess("C:/project/recipeMakret_server/resource/recipeProcess/135.txt");
-			info.setRecipeSumm("유부에 넣어 달걀찜을 하면 도시락 반찬으로 모양도 예쁘고 맛도 있는 센스만점 반찬이 된답니다~");
-
-			Point p = new Point();
-			p.setLikeCount(71);
-			p.setDisLikeCount(52);
-			p.setRecipeCode(135);
-			info.setPoint(p);
+			
+			//test_MyReviewView(dio);
+			//test_MyReviewViewList(dio);
+			test_recipeCodeReviewViewList(dio);
+//			CustomerShare.loginedId = "backym";
+//			RecipeInfo info = new RecipeInfo();
+//			info.setRecipeCode(135);
+//			info.setIngredients(null);
+//			info.setRecipeName("유부계란찜");
+//			info.setRecipePrice(3000);
+//			info.setRecipeProcess("C:/project/recipeMakret_server/resource/recipeProcess/135.txt");
+//			info.setRecipeSumm("유부에 넣어 달걀찜을 하면 도시락 반찬으로 모양도 예쁘고 맛도 있는 센스만점 반찬이 된답니다~");
+//
+//			Point p = new Point();
+//			p.setLikeCount(71);
+//			p.setDisLikeCount(52);
+//			p.setRecipeCode(135);
+//			info.setPoint(p);
 
 			/* test_favorite */
-
 			// FavoriteListView view = new FavoriteListView(dio);
-
 			// Favorite f = new Favorite();
 			// f.setCustomerId(CustomerShare.loginedId);
 			// f.setRecipeInfo(info);
@@ -51,12 +53,36 @@ public class TestView {
 			// view.showReviewListByRecipeCodeView(134);
 
 			/* review insert Test */
-			AddReviewView view = new AddReviewView(dio);
-			view.insertReviewView(info);
+//			AddReviewView view = new AddReviewView(dio);
+//			view.insertReviewView(info);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private static void test_MyReviewView (DataIO dio) {
+		MyReviewListView view = new MyReviewListView(dio);
+		CustomerShare.loginedId = "kosj";
+		view.showMyReviewListView(CustomerShare.loginedId);
+	}
+	
+	private static void test_MyReviewViewList (DataIO dio) {
+		MyReviewListView view = new MyReviewListView(dio);
+		CustomerShare.loginedId = "kosj";
+		List<Review> list = view.searchReviewList(CustomerShare.loginedId);
+		
+		for (Review r : list ) {
+			System.out.println(r.getCustomerId());
+			System.out.println(r.getReviewComment());
+			System.out.println(r.getReviewDate());
+		}
+	}
+	
+	private static void test_recipeCodeReviewViewList (DataIO dio) {
+		ReviewListView view = new ReviewListView(dio);
+		int recipeCode = 134;
+		view.showReviewListByRecipeCodeView(recipeCode);
 	}
 
 } // end class TestView()
