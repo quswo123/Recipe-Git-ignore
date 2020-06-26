e0d2b837ceb83cfae3ff9f432994363e2f1b3da7
DESKTOP-4QNPDLJ\Hailey <yi.hailey@gmail.com>
2020-06-26 17:25:23 +0900

    안녕

--- recipe_server/src/com/recipe/control/RdFrontThread.java
+++ recipe_server/src/com/recipe/control/RdFrontThread.java
@@ -226,10 +226,7 @@ public class RdFrontThread implements Runnable{
 		try {
 			
 			recipeInfo = control.searchByName(recipeName);
-<<<<<<< HEAD
-=======
 
->>>>>>> 8f9fb8c0290f7a7007969d5506786cf41df4de29
 			dio.sendSuccess();
 			dio.send(recipeInfo);
 		} catch (FindException e) {
